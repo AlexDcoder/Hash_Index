401176dc3844c3a051965d91b44efdd542e6b4b2
Vinishows <104833002+Vinishows@users.noreply.github.com>
2025-03-12 02:14:25 -0300

    Tratamento colisão e overflow

--- app/src/bucket.py
+++ app/src/bucket.py
@@ -3,11 +3,15 @@ from typing import Any
 
 @dataclass
 class Bucket:
-    max_tuplas: int # número máximo de tuplas permitidas no bucket
-    entries: list[dict[str, Any]] = field(default_factory=list) # onde vai armazenar os objetos de entrada (cada entrada é uma tupla com chave e página)
-    
+    max_tuplas: int  # Número máximo de tuplas permitidas no bucket
+    entries: list[dict[str, Any]] = field(default_factory=list)  # Entradas principais
+    overflow_entries: list[dict[str, Any]] = field(default_factory=list)  # Lista de overflow
+
     def adicionar_entrada(self, chave, pagina):
         self.entries.append({"chave": chave, "pagina": pagina})
 
+    def adicionar_overflow(self, chave, pagina):
+        self.overflow_entries.append({"chave": chave, "pagina": pagina})  # Encadeamento separado
+
     def has_overflow(self):
-        return len(self.entries) > self.max_tuplas
+        return len(self.entries) > self.max_tuplas or len(self.overflow_entries) > 0
